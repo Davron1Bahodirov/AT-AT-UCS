aee079daa932dce7a1b8f81b238d85830979fd05
Doston <dostonsayfulloyev>
2021-12-04 10:01:35 +0500

    add vue

---
+++ main.js
@@ -0,0 +1,27 @@
+
+new Vue({
+    el: ".container",
+    data: {
+        products: [
+            {
+                id: 1,
+                name: "samsung",
+                price: 200,
+                image: "https://images.pexels.com/photos/112460/pexels-photo-112460.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+            },
+            {
+                id: 2,
+                name: "samsung",
+                price: 200,
+                image: "https://images.pexels.com/photos/112460/pexels-photo-112460.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+            },
+            {
+                id: 3,
+                name: "samsung",
+                price: 200,
+                image: "https://images.pexels.com/photos/112460/pexels-photo-112460.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+            },
+
+        ]
+    }
+})
