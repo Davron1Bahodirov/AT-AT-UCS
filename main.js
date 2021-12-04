2a8986a59be96e053f558103ce68e011583d1af9
Doston <dostonsayfulloyev>
2021-12-04 10:15:04 +0500

    add v-for

--- main.js
+++ main.js
@@ -20,8 +20,7 @@ new Vue({
                 name: "samsung",
                 price: 200,
                 image: "https://images.pexels.com/photos/112460/pexels-photo-112460.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-            },
-
+            }
         ]
     }
 })
